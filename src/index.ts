19822acefcca8d036c1feaa8a8ced0e627d93246
Rodrigo Chumpitaz <chumpitazr130@gmail.com>
2025-09-22 02:14:23 -0500

    feat: add dotenv for environment variable management and update server to use process.env.PORT

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import app from "./app"
 import { seedDatabase } from "./seeders";
 import { DatabaseConnection } from "./shared";
+import 'dotenv/config';
 
 (async () => {
-    app.listen(Bun.env.PORT, () => {
-        console.log(`Server is running on http://localhost:${Bun.env.PORT}`);
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on http://localhost:${process.env.PORT}`);
     });
     await DatabaseConnection.default();
     await seedDatabase();
